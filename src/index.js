ddbb75a38da721d70d6c1ed194f7297b970c9882
arminkoeck <armin.koeck@gmail.com>
2024-04-25 13:30:03 +0200

    Create mobile dropdown menu

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
 import "./style.css";
+
+const dropdownItems = document.querySelectorAll(".dropdown");
+
+dropdownItems.forEach((item) => {
+  item.classList.add("hidden");
+  item.addEventListener("click", () => {
+    if (item.classList.contains("hidden")) {
+      dropdownItems.forEach((item) => {
+        item.classList.add("hidden");
+      });
+      item.classList.remove("hidden");
+    } else {
+      item.classList.add("hidden");
+    }
+  });
+});
